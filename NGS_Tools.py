2c96b85e3482a4f474244dea167421b27a36c930
愿乘风归去 <moqiqin@live.com>
2024-01-20 21:03:04 +0800

    Update NGS_Tools.py
    刷新版号

--- NGS_Tools.py
+++ NGS_Tools.py
@@ -17,7 +17,7 @@ class MyMainWin(QMainWindow, Ui_MainWindow):
     def __init__(self, parent = None):
         super(MyMainWin, self).__init__(parent)
         self.setupUi(self)
-        self.version = "1.5.1"
+        self.version = "1.5.2"
         self.setWindowTitle(self.windowTitle() + " v"+self.version)
         self.checkUpdate()
 
