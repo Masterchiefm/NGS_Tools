a870d5b7ebff52cf7f40d8e8587ef923d618e0e7
愿乘风归去 <moqiqin@live.com>
2024-01-20 19:56:03 +0800

    Update background_task.py
    Fix error in demiltiplex

--- background_task.py
+++ background_task.py
@@ -10,7 +10,42 @@ def getUid():
     uid = uuid.uuid4()
     return uid
 
+class monitorThread(QThread):
+    """For demultiplex"""
+    finished = pyqtSignal(str)
+    updated = pyqtSignal(int)
+    uid = ""
 
+    def __init__(self,uid = "sdf"):
+        self.uid = uid
+        super(monitorThread,self).__init__()
+
+
+
+    def monitor(self):
+        '''统计完成的文件数，从而获取进度'''
+        self.task_count_path = "/tmp/" + self.uid
+        path = self.task_count_path
+        try:
+            finnished_task = os.listdir(path)
+            finnished_num = len(finnished_task)
+        except:
+            return 0
+        return finnished_num
+
+    def run(self) -> None:
+        finnished_sum = 0
+        print("sdfsdafdsaf " + self.uid)
+        while True:
+            current_sum = int(self.monitor())
+            if finnished_sum != current_sum:
+                finnished_sum = current_sum
+                self.updated.emit(finnished_sum)
+                print(finnished_sum)
+            else:
+                print("on going..." + str(current_sum))
+            time.sleep(0.5)
+            
 
 class bgThread(QThread):
     finished = pyqtSignal(str)
